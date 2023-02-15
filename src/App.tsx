5cb2d7b3f2525f12fc99342d1377a29454948aea
John <john.jaenckel@icloud.com>
2023-02-15 15:29:16 +0100

    added footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import GameScreen from "./components/game/GameScreen";
+import GameScreen from "./Pages/GameScreen";
 import Navbar from "./components/Navbar";
 import PageNotFound from "./Pages/NotFound";
+import Footer from "./components/Footer";
 
 import blob1 from "./assets/blob1.svg";
 import blob2 from "./assets/blob2.svg";
 
 function App() {
   return (
-    //navbar??
-    //später route zum GameScreen
     <Router>
       <Navbar />
       <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/game" element={<GameScreen />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/game" element={<GameScreen />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
         {/* <img src={blob1} className="blob1" alt="blob" /> */}
         {/* <img src={blob2} className="blob2" alt="blob" /> */}
       </main>
-      </Router>
-    
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+        <div className="container">
+            <div className="content has-text-centered">
+            <p>
+                <strong>kwizz</strong> by <a href="https://github.com/leSullivan">John Jänckel</a>. The source code is licensed 
+                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+            </p>
+            </div>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer
